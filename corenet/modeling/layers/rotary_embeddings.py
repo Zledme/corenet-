919092e2e6f57bb83682c66670242b55ae12a8fb
zledme <zledme@tutatnota.com>
2024-07-20 13:09:57 +0000

    change rope

--- corenet/modeling/layers/rotary_embeddings.py
+++ corenet/modeling/layers/rotary_embeddings.py
@@ -23,21 +23,66 @@ def _negate_half(x: Tensor) -> Tensor:
     return torch.cat((-x2, x1), dim=-1)
 
 
-def _apply_rotary_pos_emb(x: Tensor, pos_sin: Tensor, pos_cos: Tensor) -> Tensor:
-    """
-    Applies rotary positional embeddings to the input tensor.
+class RoMaFunctions:
+    @staticmethod
+    def e_to_q(roll, pitch, yaw):
+        cy = torch.cos(yaw * 0.5)
+        sy = torch.sin(yaw * 0.5)
+        cp = torch.cos(pitch * 0.5)
+        sp = torch.sin(pitch * 0.5)
+        cr = torch.cos(roll * 0.5)
+        sr = torch.sin(roll * 0.5)
+    
+        w = cr * cp * cy - sr * sp * sy
+        x = sr * cp * cy + cr * sp * sy
+        y = cr * sp * cy - sr * cp * sy
+        z = cr * cp * sy + sr * sp * cy
+    
+        return torch.stack([w, x, y, z], dim=0)
 
-    Args:
-        x: Input tensor.
-        pos_sin: Sine positional embeddings. The shape of 'pos_sin' embeddings is
-                [1, 1, number of key tokens, model dimension].
-        pos_cos: Cosine positional embeddings. The shape of 'pos_cos' embeddings is
-                [1, 1, number of key tokens, model dimension].
+    @staticmethod
+    def quat_conj(quaternion):
+        return torch.stack([quaternion[0], -quaternion[1], -quaternion[2], -quaternion[3]], dim=0)
 
-    Returns:
-        Tensor with rotary positional embeddings applied.
-    """
-    return (x * pos_cos) + (_negate_half(x) * pos_sin)
+    @staticmethod
+    def quaternion_multiply(q1, q2):
+        w1, x1, y1, z1 = q1
+        w2, x2, y2, z2 = q2
+
+        w_res = w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2
+        x_res = w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2
+        y_res = w1 * y2 - x1 * z2 + y1 * w2 + z1 * x2
+        z_res = w1 * z2 + x1 * y2 - y1 * x2 + z1 * w2
+
+        return torch.stack([w_res, x_res, y_res, z_res], dim=0)
+
+    @staticmethod
+    def qvq_multiply(quaternion, a, b, c):
+        v_quaternion = torch.stack([ torch.zeros_like(a), a,b,c], dim = 0)
+        q_conjugate = RoMaFunctions.quat_conj(quaternion)
+        intermediate = RoMaFunctions.quaternion_multiply(quaternion, v_quaternion)
+        rotated_vector_quaternion = RoMaFunctions.quaternion_multiply(intermediate, q_conjugate)
+        # breakpoint()
+        x_res, y_res, z_res = rotated_vector_quaternion[1:]
+
+        return torch.stack([x_res, y_res, z_res], dim = -1).flatten(3)
+
+def precompute_freqs_cis(dim: int, end: int, theta: float = 10000.0):
+    freqs = 1.0 / (theta ** (torch.arange(0, dim, 3)[: (dim // 3)].float() / dim))
+    t = torch.arange(end, device=freqs.device)  # type: ignore
+    freqs = torch.outer(t, freqs).float()  # type: ignore
+    freqs_cos = torch.cos(freqs)  # real part
+    freqs_sin = torch.sin(freqs)  # imaginary part
+    return freqs, freqs
+
+def reshape_for_broadcast(freqs_cis: torch.Tensor, x: torch.Tensor):
+    ndim = x.ndim
+    assert 0 <= 1 < ndim
+    # breakpoint()
+    assert freqs_cis.shape == (x.shape[1], x.shape[-1])
+    shape = [d if i == 1 or i == ndim - 1 else 1 for i, d in enumerate(x.shape)]
+    return freqs_cis.view(shape)
+
 
 
 class RotaryEmbedding(torch.nn.Module):
@@ -120,18 +165,21 @@ class RotaryEmbedding(torch.nn.Module):
             # The shape of 'emb' is [number of key tokens, model dimension]
             emb = torch.cat((pos_index_theta, pos_index_theta), dim=-1)
 
+
             # the shape of cos and sin embeddings is [number of key tokens, model_dim]
-            cos_emb = emb.cos().to(dtype=key_dtype, device=key_device)
-            sin_emb = emb.sin().to(dtype=key_dtype, device=key_device)
+            cos_emb = emb.to(dtype=key_dtype, device=key_device)
+            sin_emb = emb.to(dtype=key_dtype, device=key_device)
 
             # the shape of cached cos and sin embeddings is [1, 1, number of key tokens, model_dim]
             self._cached_cos = cos_emb[None, None, :, :]
             self._cached_sin = sin_emb[None, None, :, :]
 
+    
+
     def forward(
         self,
-        query: torch.Tensor,
-        key: torch.Tensor,
+        xq: torch.Tensor,
+        xk: torch.Tensor,
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         """
         The forward function of RoPE embeddings.
@@ -150,13 +198,13 @@ class RotaryEmbedding(torch.nn.Module):
             The RoPE embedding computation is done in full-precision. After the computation, input query and key tensors
             are casted to original input datatype.
         """
-        dim = key.shape[-1]
-        key_len = key.shape[2]
-        query_len = query.shape[2]
+        dim = xk.shape[-1]
+        key_len = xk.shape[2]
+        query_len = xq.shape[2]
 
         assert dim == self.model_dim
-        assert key.device == query.device
-        assert key.dtype == query.dtype
+        assert xk.device == xq.device
+        assert xk.dtype == xq.dtype
 
         # In the context of self-attention, the lengths of keys and queries are equal.
         # However, in generation tasks, such as predicting the next token in a sequence, the lengths of keys and queries
@@ -173,15 +221,23 @@ class RotaryEmbedding(torch.nn.Module):
         self._compute_sin_cos_embeddings(
             key_len, key_device=key_float.device, key_dtype=key_float.dtype
         )
-        query_float = _apply_rotary_pos_emb(
-            x=query_float,
-            pos_sin=self._cached_sin[..., key_len - query_len : key_len, :],
-            pos_cos=self._cached_cos[..., key_len - query_len : key_len, :],
-        )
-        key_float = _apply_rotary_pos_emb(
-            x=key_float,
-            pos_sin=self._cached_sin[..., :key_len, :],
-            pos_cos=self._cached_cos[..., :key_len, :],
-        )
 
-        return query_float.type_as(query), key_float.type_as(key)
+        xq_a, xq_b, xq_c = xq.float().reshape(xq.shape[:-1] + (-1, 3)).unbind(-1)
+        xk_a, xk_b, xk_c = xk.float().reshape(xk.shape[:-1] + (-1, 3)).unbind(-1)
+    
+        # reshape freqs_cos and freqs_sin for broadcasting
+        freqs = reshape_for_broadcast(self._cached_cos, xq_a)
+        # freqs_sin = reshape_for_broadcast(freqs_sin, xq_a)
+
+        rot_axis = torch.tensor([1.0], device=freqs.device) / torch.sqrt(torch.tensor(3.0))
+        axis = rot_axis / torch.norm(rot_axis, dim=-1, keepdim=True)
+        half_angle = freqs / 2.0
+        sin_half = torch.sin(half_angle)
+        # return torch.cat([torch.cos(half_angle), axis * sin_half], dim=-1)
+        quaternion = torch.stack([torch.cos(half_angle), rot_axis * sin_half, rot_axis * sin_half, rot_axis * sin_half], dim=0)
+        
+        # quaternion = RoMaFunctions.e_to_q(half_angle, half_angle, half_angle)
+        
+        xq_out = RoMaFunctions.qvq_multiply(quaternion, xq_a, xq_b, xq_c)
+        xk_out = RoMaFunctions.qvq_multiply(quaternion, xk_a, xk_b, xk_c)
+        return xq_out.type_as(xq), xk_out.type_as(xk)
